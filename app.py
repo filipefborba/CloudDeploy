8060df83cd6ba7c82d2b48a2e5e9d445f871fc54
Filipe Foschini Borba <filipefborba@gmail.com>
2018-11-28 00:00:51 -0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, Response
-from tasks import *
 import json
 import os
+import firebase_admin
+from firebase_admin import db
 
 app = Flask(__name__)
+cred = firebase_admin.credentials.Certificate("./firebase_keys.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://cloudprojeto-27847.firebaseio.com/'
+})
+ref = db.reference('/')
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
@@ -11,38 +17,36 @@ def healthcheck():
 
 @app.route('/Tarefa', methods=['GET', 'POST'])
 def Tarefa():
-    global task_dict
-    global id_dict
     if request.method == 'POST':
         data = request.get_json(force=True)
-        t = Tarefas(id_dict, data["nome"])
-        task_dict[id_dict] = t
-        id_dict += 1
-        return json.dumps([task.__dict__ for task in list(task_dict.values())]), 201
+        tarefas_ref = ref.child('tarefas')
+        add = tarefas_ref.push(data)
+        return add.key, 201
     else:
-        return json.dumps([task.__dict__ for task in list(task_dict.values())]), 200
+        tarefas_ref = ref.child('tarefas').get()
+        return json.dumps(tarefas_ref), 200
 
 @app.route('/Tarefa/<id_key>', methods=['GET', 'PUT', 'DELETE'])
 def tarefa_id(id_key):
-    global task_dict
-    global id_dict
-    id = int(id_key)
-
     if request.method == 'PUT':
         data = request.get_json(force=True)
-        if id in task_dict:
-            t = Tarefas(id, data["nome"])
-            task_dict[id] = t
-            return Response(status=200)
+        tarefas_ref = ref.child('tarefas')
+        update_ref = tarefas_ref.child(id_key)
+        if (update_ref.get()):
+            update_ref.update(data)
+            return json.dumps(tarefas_ref.child(id_key).get()), 200
         else:
-            return Response(status=204)
+            return "None", 404
     elif request.method == 'DELETE':
-        if id in task_dict:
-            del task_dict[id]
-            return Response(status=200)
-        else:
-            return Response(status=204)
+        tarefas_ref = ref.child('tarefas')
+        delete_ref = tarefas_ref.child(id_key).delete()
+        return "OK", 200
     else:
-        return json.dumps(task_dict[int(id)].__dict__)
+        tarefas_ref = ref.child('tarefas')
+        get = tarefas_ref.child(id_key).get()
+        if (get):
+            return json.dumps(get), 200
+        else:
+            return "None", 404
 
 app.run(host=os.environ["APP_URL"], port=5000)
